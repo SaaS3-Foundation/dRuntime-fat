fbbd8dbd1532efe9097343dff70bdca7d18f0a3e
songtianyi <tianyi@saas3.io>
2022-12-04 22:16:29 +0800

    feat: add test case

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ mod sample_oracle {
     use primitive_types::H160;
     use scale::{Decode, Encode};
 
-    use abi::ABI;
+    use abi::{encode::str_chunk32_bytes, ABI};
     use pink::http_get;
     use primitive_types::U256;
     use serde_json;
@@ -55,7 +55,7 @@ mod sample_oracle {
         apikey: Option<String>,
     }
 
-    #[derive(Encode, Decode, Debug)]
+    #[derive(Encode, Decode, Debug, PartialEq)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         BadOrigin,
@@ -63,16 +63,25 @@ mod sample_oracle {
         BadAbi,
         FailedToGetStorage,
         FailedToDecodeStorage,
-        FailedToEstimateGas,
+        FailedToDecodeOracleRequest,
         FailedToDecodeParams,
         FailedToDecodeResBody,
         FailedToDecodeByPath,
+        FailedToGetInternalPath,
         Web2StatusError,
-        MultiplyTimesOverflow,
+        TimesTooSmall,
         FailedToCreateRollupSession,
         FailedToFetchLock,
         FailedToReadQueueHead,
         FailedToDecodeNumberFromJson,
+        TypeNotSet,
+        InvalidType,
+        InvalidRootValue,
+        NumberSignNotMatch,
+        EncodeNonDecimalNumberTo256Failed,
+        EncodeStringTo32BytesFailed,
+        TimesParseFailed,
+        NotANumberOrString,
     }
 
     type Result<T> = core::result::Result<T, Error>;
@@ -105,89 +114,124 @@ mod sample_oracle {
             Ok(())
         }
 
-        #[ink(message)]
-        pub fn test_httpget(&self, url: String) -> Result<()> {
-            let resp = http_get!(url);
-            if resp.status_code != 200 {
-                return Err(Error::Web2StatusError);
-            }
-
-            #[cfg(feature = "std")]
-            println!("Got response {:#?}", resp.body);
-
-            let body = resp.body;
-
-            //let mr_str = String::from_utf8(body);
-            //let mr = mr_str.unwrap().parse::<u32>().unwrap();
-
-            let root = serde_json::from_slice::<serde_json::Value>(&body)
-                .or(Err(Error::FailedToDecodeResBody));
-
-            #[cfg(feature = "std")]
-            println!("Got response {:#?}", root);
-
-            if root.is_err() {
-                return Err(root.err().unwrap());
-            }
-            let _path = "ethereum.usd";
-            let v = _path.split(".").fold(root, |v, p| {
+        fn read_by_path(&self, v: serde_json::Value, path: &str) -> Result<serde_json::Value> {
+            let v = path.split(".").fold(Ok(v), |v, p| {
                 #[cfg(feature = "std")]
                 println!("p: {}", p);
 
                 let v = v?.get(p).ok_or(Error::FailedToDecodeByPath).cloned();
                 v
-            });
-            if v.is_err() {
-                return Err(v.err().unwrap());
-            }
-            let v = v.unwrap();
+            })?;
 
             #[cfg(feature = "std")]
-            println!("Decoded data {:#?}", v);
+            println!("path {} value {:#?}", path, v);
 
-            let _type = "number";
+            Ok(v)
+        }
+
+        fn encode_from_string_to_256(
+            &self,
+            s: String,
+            signed: bool,
+            _times: u32,
+        ) -> Result<ethabi::Token> {
+            let is_signed = s.starts_with("-");
+            if is_signed && !signed {
+                // eg. try encode "-3" to u256
+                return Err(Error::NumberSignNotMatch);
+            }
+            let pos = s.find(".");
+            if pos != None {
+                // offset count
+                let y = s.len() - pos.unwrap() - 1;
+
+                if 10_u32.pow(y as u32) > _times {
+                    return Err(Error::TimesTooSmall);
+                }
+
+                // remove decimal point
+                let mut s = s.to_owned();
+                s.retain(|c| c != '.' && c != '-');
+
+                // alreay multiply by 10^y
+                let mut v = s.parse::<u64>().unwrap();
+
+                // times must >= 10^y
+                v = v * (_times / 10_u32.pow(y as u32)) as u64;
+
+                // encode to u256
+                if is_signed {
+                    return Ok(ethabi::Token::Int(U256::from(v)));
+                } else {
+                    return Ok(ethabi::Token::Uint(U256::from(v)));
+                }
+            } else {
+                // not a decimal number
+                let mut s = s.to_owned();
+                s.retain(|c| c != '-');
+
+                #[cfg(feature = "std")]
+                println!("s: {}", s);
+
+                if signed {
+                    s.parse::<i64>()
+                        .map(|x| ethabi::Token::Int(U256::from(x)))
+                        .map_err(|_| Error::EncodeNonDecimalNumberTo256Failed)
+                } else {
+                    s.parse::<u64>()
+                        .map(|x| ethabi::Token::Uint(U256::from(x)))
+                        .map_err(|_| Error::EncodeNonDecimalNumberTo256Failed)
+                }
+            }
+        }
+
+        fn encode_answer(
+            &self,
+            v: serde_json::Value,
+            _type: &str,
+            _times: u32,
+        ) -> Result<ethabi::Token> {
             match v {
                 serde_json::Value::Number(n) => {
-                    let s = n.to_string();
-                    let _times = 100;
-                    let x = s.find(".");
-                    if x == None {
-                        // no decimal point
-                    } else {
-                        let y = s.len() - x.unwrap() - 1;
-
-                        // check if _times is set or not
-                        if 10_u32.pow(y as u32) > _times {
-                            return Err(Error::MultiplyTimesOverflow);
+                    return match _type {
+                        "string" => Ok(ethabi::Token::String(n.to_string())),
+                        "string32" => {
+                            let chunk = str_chunk32_bytes(&n.to_string())
+                                .map_err(|_| Error::EncodeStringTo32BytesFailed)?;
+                            Ok(ethabi::Token::FixedBytes(chunk))
                         }
-
-                        let mut s = s.to_owned();
-                        s.retain(|c| c != '.');
-                        // alread multiply by 10^y
-                        let mut s = s.parse::<u64>().unwrap();
-                        s = s * (_times / 10_u32.pow(y as u32)) as u64;
-
-                        #[cfg(feature = "std")]
-                        println!("s: {}", s);
-                    }
+                        "uint256" => {
+                            Ok(self.encode_from_string_to_256(n.to_string(), false, _times)?)
+                        }
+                        "int256" => {
+                            Ok(self.encode_from_string_to_256(n.to_string(), true, _times)?)
+                        }
+                        _ => Err(Error::InvalidType),
+                    };
                 }
                 serde_json::Value::String(s) => {
                     #[cfg(feature = "std")]
                     println!("String: {}", s);
+
+                    return match _type {
+                        "string" => Ok(ethabi::Token::String(s)),
+                        "string32" => {
+                            let chunk = str_chunk32_bytes(&s.to_string())
+                                .map_err(|_| Error::EncodeStringTo32BytesFailed)?;
+                            Ok(ethabi::Token::FixedBytes(chunk))
+                        }
+                        "uint256" => Ok(self.encode_from_string_to_256(s, false, _times)?),
+                        "int256" => Ok(self.encode_from_string_to_256(s, true, _times)?),
+                        _ => Err(Error::InvalidType),
+                    };
                 }
                 _ => {
                     #[cfg(feature = "std")]
-                    println!("Not a number");
+                    println!("Not a number or string");
+
+                    return Err(Error::NotANumberOrString);
                 }
             }
-            // TODO use macro to generate the code
-            // 1. get path field
-            // 2. generate the code
-
-            // offchain to onchain 1.0 string x 10000
-            // onchain to offchain / 10000
-            // U256
-            Ok(())
         }
 
         fn handle_req(&self) -> Result<Option<RollupResult>> {
@@ -215,9 +259,6 @@ mod sample_oracle {
                 Error::FailedToReadQueueHead
             })?;
 
-            #[cfg(feature = "std")]
-            println!("raw_item: {:?}, idx: {:?}", raw_item, idx);
-
             let raw_item = match raw_item {
                 Some(v) => v,
                 _ => {
@@ -238,7 +279,7 @@ mod sample_oracle {
 
             let (rid, parameter_abi_bytes) = match decoded.as_slice() {
                 [ethabi::Token::Uint(reqid), ethabi::Token::Bytes(content)] => (reqid, content),
-                _ => return Err(Error::FailedToDecodeStorage),
+                _ => return Err(Error::FailedToDecodeOracleRequest),
             };
 
             #[cfg(feature = "std")]
@@ -250,18 +291,51 @@ mod sample_oracle {
             #[cfg(feature = "std")]
             println!("Got decoded params abi {:?}", decoded_abi);
 
+            // build url suffix
             let url_suffix = decoded_abi
                 .params
                 .iter()
-                .filter(|param| !param.get_name().starts_with("_"))
+                .filter(|param| !param.get_name().starts_with("_")) // skip the internal params
                 .map(|param| param.get_name().to_string() + "=" + &param.get_value().to_string())
                 .collect::<Vec<String>>()
                 .join("&");
 
+            // path is optional, if not set, we will use the root path
+            let _path = decoded_abi
+                .params
+                .iter()
+                .find(|param| param.get_name() == "_path")
+                .get_or_insert(&abi::Param::String {
+                    name: "_path".to_string(),
+                    value: "".to_string(),
+                })
+                .get_value();
+
+            // _type is necessary
+            let _type = decoded_abi
+                .params
+                .iter()
+                .find(|param| param.get_name() == "_type")
+                .ok_or(Error::TypeNotSet)?
+                .get_value();
+
+            // _times is only necessary for float number data
+            let _times = decoded_abi
+                .params
+                .iter()
+                .find(|param| param.get_name() == "_times")
+                .get_or_insert(&abi::Param::String {
+                    name: "_times".to_string(),
+                    value: "100".to_string(),
+                })
+                .get_value()
+                .parse::<u32>()
+                .map_err(|_| Error::TimesParseFailed)?;
+
             #[cfg(feature = "std")]
             println!("Got url suffix {:?}", url_suffix);
 
-            let ApiConfig { url, apikey } =
+            let ApiConfig { url, apikey: _ } =
                 self.apiconfig.as_ref().ok_or(Error::NotConfigurated)?;
 
             let uri = url.to_owned() + "?" + &url_suffix;
@@ -277,11 +351,27 @@ mod sample_oracle {
             #[cfg(feature = "std")]
             println!("Got response {:?}", resp.body);
 
-            let body = resp.body;
-            let mr_str = String::from_utf8(body);
-            let mr = mr_str.unwrap().parse::<u32>().unwrap();
+            let root = serde_json::from_slice::<serde_json::Value>(&resp.body)
+                .or(Err(Error::FailedToDecodeResBody))?;
 
-            let answer = ethabi::encode(&[ethabi::Token::Uint(U256::from(mr))]);
+            #[cfg(feature = "std")]
+            println!("Got response {:#?}", root);
+
+            let mut v = root.clone();
+            if _path != "" {
+                v = self.read_by_path(root, &_path)?;
+            } // no path, use the root path
+
+            if v.is_array() || v.is_null() || v.is_object() {
+                // we only support number, string, bool
+                return Err(Error::InvalidRootValue);
+            }
+            let answer = self.encode_answer(v, &_type, _times)?;
+
+            #[cfg(feature = "std")]
+            println!("answer {:#?}", answer);
+
+            let answer = ethabi::encode(&[answer]);
 
             // Apply the response to request
             let payload =
@@ -317,6 +407,139 @@ mod sample_oracle {
         use super::*;
         use ink_lang as ink;
 
+        #[ink::test]
+        fn read_by_path_should_ok() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let root = serde_json::from_str::<serde_json::Value>(
+                r#"
+                {
+                    "a": {
+                        "b": {
+                            "c": 1
+                        }
+                    }
+                }
+                "#,
+            )
+            .unwrap();
+            let v = oracle.read_by_path(root, "a.b.c").unwrap();
+            assert_eq!(v, serde_json::Value::Number(1.into()));
+        }
+
+        #[ink::test]
+        fn read_by_path_should_fail() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let root = serde_json::from_str::<serde_json::Value>(
+                r#"
+                {
+                    "a": {
+                        "b": {
+                            "c": 1
+                        }
+                    }
+                }
+                "#,
+            )
+            .unwrap();
+            let v = oracle.read_by_path(root.clone(), "a.b.d");
+            assert!(v.is_err());
+            assert_eq!(v.err().unwrap(), Error::FailedToDecodeByPath);
+            let v1 = oracle.read_by_path(root, "a.b");
+            assert!(v1.unwrap().is_object());
+        }
+
+        #[ink::test]
+        fn encode_from_string_to_256_should_ok() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = oracle
+                .encode_from_string_to_256("1.23".to_string(), false, 100)
+                .unwrap();
+            assert_eq!(v, ethabi::Token::Uint(123.into()));
+        }
+
+        #[ink::test]
+        fn encode_from_string_to_256_should_ok_with_signed_decimal() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = oracle
+                .encode_from_string_to_256("-1.23".to_string(), true, 100)
+                .unwrap();
+            assert_eq!(v, ethabi::Token::Int(123.into()));
+        }
+
+        #[ink::test]
+        fn encode_from_string_to_256_should_ok_with_signed_non_decimal() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = oracle
+                .encode_from_string_to_256("-123".to_string(), true, 100)
+                .unwrap();
+            assert_eq!(v, ethabi::Token::Int(123.into()));
+        }
+
+        #[ink::test]
+        fn encode_from_string_to_256_should_fail() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = oracle.encode_from_string_to_256("1.23".to_string(), false, 10);
+            assert_eq!(v.err().unwrap(), Error::TimesTooSmall);
+        }
+
+        #[ink::test]
+        fn encode_from_string_to_256_should_ok_with_non_decimal_str() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = oracle.encode_from_string_to_256("123".to_string(), false, 10);
+            assert_eq!(v.unwrap(), ethabi::Token::Uint(123.into()));
+        }
+
+        #[ink::test]
+        fn encode_answer_should_ok_with_string32() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = serde_json::json!({ "city": "saas3", "street": "10 Downing Street" });
+            let v = oracle.read_by_path(v, "city").unwrap();
+            let t = oracle.encode_answer(v, "string32", 100).unwrap();
+            assert_eq!(
+                t.into_bytes(),
+                ethabi::Token::String("saas3".to_string()).into_bytes()
+            );
+        }
+
+        #[ink::test]
+        fn encode_answer_should_ok_with_float() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let oracle = SampleOracle::default();
+            let v = serde_json::json!({ "city": 1.32, "street": "10 Downing Street" });
+            let v = oracle.read_by_path(v, "city").unwrap();
+            let t = oracle.encode_answer(v, "uint256", 1000).unwrap();
+            assert_eq!(
+                t.into_bytes(),
+                ethabi::Token::Uint(U256::from(1320)).into_bytes()
+            );
+        }
+
         fn consts() -> (String, H160) {
             dotenvy::dotenv().ok();
             // let rpc = env::var("RPC").unwrap();
@@ -350,15 +573,8 @@ mod sample_oracle {
                 )
                 .unwrap();
 
-            //sample_oracle.test_httpget("https://rpc.saas3.io:3301/saas3/web2/qatar2022/played?home=1. FC Union Berlin&guest=FC Augsburg".to_string()).unwrap();
-            sample_oracle
-                .test_httpget(
-                    "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
-                        .to_string(),
-                )
-                .unwrap();
-            //let res = sample_oracle.handle_req().unwrap();
-            //println!("res: {:#?}", res);
+            let res = sample_oracle.handle_req().unwrap();
+            println!("res: {:#?}", res);
         }
     }
 }
